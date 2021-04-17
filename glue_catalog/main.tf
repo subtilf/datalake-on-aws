ef9023b8ecbfc866df4b12c6d7838c6a0950510e
Subtil <felipe.subtil@outlook.com.br>
2021-04-17 20:34:42 -0300

    First implementation of glue catalog

---
+++ glue_catalog/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.main_region
+}
+
+terraform {
+    required_version = ">= 0.14"
+    backend "s3"{
+    bucket = "s3-sublab-tfstate"
+    key = "datalake/glue_catalog.tfstate"
+    region = "us-east-2"
+  }
+}
+
+module "kms_datalake_glue_catalog_key"{
+    source = "../modules/kms"
+    kms_desc = "Key used to encrypt all glue catalog"
+    tags = {
+        name  = "kms-sublab-dl-glue-catalog-lab"
+        env   = "lab"
+        owner = "terraform"
+    }
+}
+
+resource "aws_glue_catalog_database" "aws_glue_catalog_database_raw" {
+  name = "glue-catalog-datalake-lab"
+  description = "Database responsible for store all metadata relationed to each layer of datalake."
+}
+
+resource "aws_glue_data_catalog_encryption_settings" "aws_glue_data_catalog_encryption" {
+  data_catalog_encryption_settings {
+    connection_password_encryption {
+      aws_kms_key_id                       = module.kms_datalake_glue_catalog_key.kms_arn
+      return_connection_password_encrypted = true
+    }
+
+    encryption_at_rest {
+      catalog_encryption_mode = "SSE-KMS"
+      sse_aws_kms_key_id      = module.kms_datalake_glue_catalog_key.kms_arn
+    }
+  }
+}
---
+++ glue_catalog/vars.tf
@@ -0,0 +1,31 @@
+variable "main_region"{
+    default = "us-east-2"
+}
+
+variable "main_vpc"{
+    default = "vpc-0a39577fd48d8bf37"
+}
+
+variable "cidr_subnet_dmz" {
+  default = "172.50.0.0/28"
+}
+
+variable "cidr_subnet_p1" {
+  default = "172.50.1.0/27"
+}
+
+variable "cidr_subnet_p2" {
+  default = "172.50.2.0/27"
+}
+
+variable "cidr_subnet_p3" {
+  default = "172.50.3.0/27"
+}
+
+variable "cidr_vpc" {
+  default = "172.50.0.0/22"
+}
+
+variable "cidr_internet" {
+  default = "0.0.0.0/0"
+}
